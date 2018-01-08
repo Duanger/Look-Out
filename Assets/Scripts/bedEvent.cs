84fe41df19d8a569b1ff70b259801a8b1c1bba10
Unknown <aaronshenglu@gmail.com>
2018-01-08 23:56:06 +0800

    Issue with OnTriggerEnterNot working when colliding with player capsule

---
+++ Assets/Scripts/bedEvent.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class bedEvent : MonoBehaviour {
+    [SerializeField]
+    private GameObject bed;
+    
+    void Start () {
+     
+    }
+    private void OnTriggerEnter(Collider other)
+    {
+        if (Input.GetAxisRaw("Fire1") != 0)
+        {
+           
+        }
+    }
+    // Update is called once per frame
+    void Update () {
+		
+	}
+}
---
+++ Assets/Scripts/compScreenEvents.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class compScreenEvents : MonoBehaviour
+{
+
+    // Use this for initialization
+    [SerializeField]
+    private Camera computerCamera;
+    [SerializeField]
+    private RenderTexture renderText;
+    private bool compTriggerEntered = false;
+
+    void Start()
+    {
+        computerCamera.targetTexture = null;
+
+    }
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.tag == "Player")
+        {
+            if (Input.GetAxis("Fire1") != 0)
+            {
+                if (!compTriggerEntered)
+                {
+                    compTriggerEntered = true;
+                    computerCamera.targetTexture = renderText;
+                }
+                
+            }
+            if (Input.GetAxis("Fire1") == 0)
+            {
+                compTriggerEntered = false;
+            }
+        }
+
+    }
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+}
