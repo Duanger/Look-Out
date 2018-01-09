4ff5191a57009770862c5829fdf689959eaf3ff1
Unknown <aaronshenglu@gmail.com>
2018-01-09 17:58:00 +0800

    do the videoplayer final frame bs as well as figure out why input.getaxis aint working

--- Assets/Scripts/compScreenEvents.cs
+++ Assets/Scripts/compScreenEvents.cs
@@ -1,45 +1,57 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.Video;
+using UnityEngine.UI;
 
 public class compScreenEvents : MonoBehaviour
 {
 
     // Use this for initialization
+    private bool compTriggerEntered;
+
     [SerializeField]
     private Camera computerCamera;
     [SerializeField]
     private RenderTexture renderText;
-    private bool compTriggerEntered = false;
+    [SerializeField]
+    private GameObject desker;
+    [SerializeField]
+    private VideoPlayer introAnimation;
+    [SerializeField]
+    private Text terminalText;
 
     void Start()
     {
         computerCamera.targetTexture = null;
-
+        compTriggerEntered = false;
     }
     private void OnTriggerEnter(Collider other)
     {
         if (other.tag == "Player")
         {
-            if (Input.GetAxis("Fire1") != 0)
+            if (Input.GetButtonDown("Fire1"))
             {
                 if (!compTriggerEntered)
                 {
                     compTriggerEntered = true;
                     computerCamera.targetTexture = renderText;
                 }
-                
             }
-            if (Input.GetAxis("Fire1") == 0)
+            if (!Input.GetButtonDown("Fire1"))
             {
                 compTriggerEntered = false;
             }
+
         }
 
     }
     // Update is called once per frame
     void Update()
     {
+        if (!introAnimation.isPlaying && )
+        {
 
+        }
     }
 }
